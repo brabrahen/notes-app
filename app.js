23ed56d71759a931c563aa85dab836eba3129435
Brayan <brayan.goncalves@squadts.com.br>
2022-11-01 20:38:01 -0300

    file that contain the "routes"

---
+++ app.js
@@ -0,0 +1,75 @@
+const notes = require('./notes.js')
+const yargs = require('yargs')
+const chalk = require('chalk')
+
+//Costumize yargs version
+yargs.version('1.1.0')
+
+// The add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// The remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// The read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNotes(argv.title)
+    }
+})
+
+// The list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+//add, remove, read, list
+
+yargs.parse()
+
+
+
